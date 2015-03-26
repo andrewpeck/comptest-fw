188e8a726d113706cbe5465c77f464cee56f3b2b
Andrew Peck <anp@ucla.edu>
2015-03-26 12:00:17 -0700

    Fix clock inputs. Setup clock forwarding for LCT-COMP output. Instantiate FTDI Controller

--- comptest.v
+++ comptest.v
@@ -1,26 +1,5 @@
 `timescale 1ns / 1ps
 
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date:    12:10:08 12/04/2014
-// Design Name:
-// Module Name:    comptest
-// Project Name:
-// Target Devices:
-// Tool versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module comptest (
     // Data Delay Chip
     output _ddd_al,
@@ -30,19 +9,22 @@ module comptest (
 
     // Serial Interface
     input  _ft_reset,
-    input  _ft_oe,
+    output _ft_oe,
     input  _ft_txe,
     input  _ft_rxf,
-    input  ft_clk,
+    input   ft_clk,
     output _ft_wr,
     output _ft_siwu,
     output _ft_rd,
 
+    // bidirectional 8 bit data bus
+    inout   [7:0] ft_data,
+
     // Pulse Generation
-    output  pdac_sclk,
-    output  pdac_din,
+    output   pdac_sclk,
+    output   pdac_din,
     output  _pdac_en,
-    output  pulse_en,
+    output   pulse_en,
 
     input pulse_trig,
 
@@ -51,8 +33,6 @@ module comptest (
     output  cdac_din,
     output  _cdac_en,
 
-    // bidirectional 8 bit data bus
-    inout   [7:0] ft_data,
 
     // Pulse Multiplexer Control
     output  [15:0] mux_a1,
@@ -76,43 +56,59 @@ module comptest (
     output  [2:0] pktime,
     output  [1:0] pkmode,
     input   compout,
-    input   [7:0] distrip
+    input   [7:0] distrip,
+
+    output sump
 );
 
-//------------------------------------------------------------------------------
-// Inter-module connections
-//------------------------------------------------------------------------------
-
-
-//wire halfstrips_expect;
-//reg[31:0] halfstrips_errcnt;
-//reg[31:0] compout_errcnt;
+/*
+ * Inter-module connections
+ */
 
 //------------------------------------------------------------------------------
 // 40 MHz Clock Generation
 //------------------------------------------------------------------------------
 wire dcm_rst;
 wire dcm_islocked;
-wire clock40;
-wire clock100;
 
+//IBUFG CLOCK60 (.I(ft_clk),.O(clk60));
 dcm uclkgen
 (
-    // Clock in ports
-    .CLK_IN1(ft_clk), // IN
-    // Clock out ports
-    .CLK_OUT1(clock100),   // OUT
-    .CLK_OUT2(clock40),    // OUT
-    // Status and control signals
+    .CLK_IN1(ft_clk), // Clock in ports
+    .CLK_OUT1(clk60), // Clock out ports
+    .CLK_OUT2(clk40), // Clock out ports
     .RESET(dcm_rst),       // IN
     .LOCKED(dcm_islocked)
 );
 
+// Clock forwarding circuit using the double data-rate register
+//        Spartan-3E/3A/6
+// Xilinx HDL Language Template, version 14.7
 
-//------------------------------------------------------------------------------
-// Comparator Injector
-//------------------------------------------------------------------------------
+ODDR2 #(
+    .DDR_ALIGNMENT("NONE"), // Sets output alignment to "NONE", "C0" or "C1"
+    .INIT(1'b0),            // Sets initial state of the Q output to 1'b0 or 1'b1
+    .SRTYPE("SYNC")         // Specifies "SYNC" or "ASYNC" set/reset
+    ) clock_forward_inst (
+        .Q(lctclk),           // 1-bit DDR output data
+        .C0(clk40),           // 1-bit clock input
+        .C1(~clk40),         // 1-bit clock input
+        .CE(!lctrst),            // 1-bit clock enable input
+        .D0(1'b0),            // 1-bit data input (associated with C0)
+        .D1(1'b1),            // 1-bit data input (associated with C1)
+        .R(!lctrst)          // 1-bit reset input
+        //.S(1'b1) // 1-bit set input
+    );
 
+    // End of clock_forward_inst instantiation
+
+
+
+/*
+ * Comparator Injector
+ */
+
+// Intermodule Connections
 wire [31:0] halfstrips;
 wire [31:0] halfstrips_expect;
 
@@ -132,141 +128,156 @@ wire [3:0] pulse_width;
 wire fire_pulse;
 
 comparator_injector u_comparator_injector (
-    .halfstrips                           ( halfstrips[31:0]        ) ,
-    .halfstrips_expect                    ( halfstrips_expect[31:0] ) ,
-    .halfstrips_errcnt                    ( halfstrips_errcnt[31:0] ) ,
-    .compout_errcnt                       ( compout_errcnt[31:0]    ) ,
-    .compout                              ( compout                 ) ,
-    .compout_expect                       ( compout_expect          ) ,
-    .compout_errcnt_rst                   ( compout_errcnt_rst      ) ,
-    .halfstrips_errcnt_rst                ( halfstrips_errcnt_rst   ) ,
-    .compin_inject                        ( compin_inject           ) ,
-    .compin                               ( compin                  ) ,
-    .fire_pulse                           ( fire_pulse              ) ,
-    .pulser_ready                         ( pulser_ready            ) ,
-    .bx_delay                             ( bx_delay                ) ,
-    .pulse_width                          ( pulse_width             ) ,
-    .pulse_en                             ( pulse_en                ) ,
-    .clock40                              ( clock40                 )
-                                                                    ) ;
+    .halfstrips                           ( halfstrips[31:0]         ),
+    .halfstrips_expect                    ( halfstrips_expect[31:0]  ),
+    .halfstrips_errcnt                    ( halfstrips_errcnt[31:0]  ),
+    .compout_errcnt                       ( compout_errcnt           ),
+    .compout                              ( compout                  ),
+    .compout_expect                       ( compout_expect           ),
+    .compout_errcnt_rst                   ( compout_errcnt_rst       ),
+    .halfstrips_errcnt_rst                ( halfstrips_errcnt_rst    ),
+    .compin_inject                        ( compin_inject            ),
+    .compin                               ( compin                   ),
+    .fire_pulse                           ( fire_pulse               ),
+    .pulser_ready                         ( pulser_ready             ),
+    .bx_delay                             ( bx_delay                 ),
+    .pulse_width                          ( pulse_width              ),
+    .pulse_en                             ( pulse_en                 ),
+    .clk                                  ( clk40                    ));
+
+/*
+ *  FT245 USB-to-Serial Converter
+ */
+
+// Inter-module connects
+wire _serial_wr;
+wire _serial_rd;
+
+wire [7:0] ft_byte_in;
+wire [7:0] ft_byte_out;
+
+ft245 u_ft245 (
+    .clk          ( clk60       ), // 60 MHz FT232H clock
+
+    ._txe         (_ft_txe      ), // Can TX
+    ._rxf         (_ft_rxf      ), // Can RX
+
+    ._rd          (_ft_rd       ), // Read Must Be low to be able to read
+    ._wr          (_ft_wr       ), // Write must be low to write to usb
+
+    ._oe          (_ft_oe       ), // Output enable, high to write to USB
+    ._reset       (_ft_reset    ), // FTDI Reset, active low
+
+    .data         ( ft_data     ), // Bidirectional FIFO data
+
+    ._write_data  (_serial_wr   ), // put low if you want to write data
+    ._read_data   (_serial_rd   ), // put low if you want to read data
+
+    .data_to_pc   ( ft_byte_in ), // data to be written to pc
+    .data_to_fpga ( ft_byte_out)  // data to be read out from pc
+);
 
 
-//------------------------------------------------------------------------------
-// Serial Port
-//------------------------------------------------------------------------------
+/*
+ * VME Style Serial Interface
+ */
 
+wire triad_perist1;
+wire [3:0] triad_persist;
 // Instantiate the module
 serial u_serial            (
-    .adc_sclk              ( adc_sclk                ) ,
-    ._adc_cs               ( _adc_cs                 ) ,
-    .adc_mosi              ( adc_mosi                ) ,
-    .adc_miso              ( adc_miso                ) ,
-    ._pdac_en              ( _pdac_en                ) ,
-    .pdac_din              ( pdac_din                ) ,
-    .pdac_sclk             ( pdac_sclk               ) ,
-    .bx_delay              ( bx_delay[2:0]           ) ,
-    .pulse_width           ( pulse_width[3:0]        ) ,
-    .fire_pulse            ( fire_pulse              ) ,
-    .pulser_ready          ( pulser_ready            ) ,
-    .halfstrips            ( halfstrips[31:0]        ) ,
-    .halfstrips_expect     ( halfstrips_expect[31:0] ) ,
-    .halfstrips_errcnt     ( halfstrips_errcnt[31:0] ) ,
-    .halfstrips_errcnt_rst ( halfstrips_errcnt_rst   ) ,
-    .compout_expect        ( compout_expect          ) ,
-    .compout_errcnt        ( compout_errcnt[31:0]    ) ,
-    .compout_errcnt_rst    ( compout_errcnt_rst      ) ,
-    .compin_inject         ( compin_inject           ) ,
-    .pktime                ( pktime[2:0]             ) ,
-    .pkmode                ( pkmode[1:0]             ) ,
-    .compout               ( compout                 ) ,
-    .compin                ( compin                  ) ,
-    .lctclk                ( lctclk                  ) ,
-    .lctrst                ( lctrst                  ) ,
-    ._cdac_en              ( _cdac_en                ) ,
-    .cdac_din              ( cdac_din                ) ,
-    .cdac_sclk             ( cdac_sclk               ) ,
-    .mux_a0                ( mux_a0[15:0]            ) ,
-    .mux_a1                ( mux_a1[15:0]            ) ,
-    .mux_a0_next           ( mux_a0_next             ) ,
-    .mux_a1_next           ( mux_a1_next             ) ,
-    .mux_a0_prev           ( mux_a0_prev             ) ,
-    .mux_a1_prev           ( mux_a1_prev             ) ,
-    ._ddd_al               ( _ddd_al                 ) ,
-    .ddd_mosi              ( ddd_mosi                ) ,
-    .ddd_miso              ( ddd_miso                ) ,
-    .ddd_sclk              ( ddd_sclk                ) ,
-    .clock40               ( clock40                 ) ,
-    ._ft_reset             ( _ft_reset                ) ,
-    ._ft_oe                ( _ft_oe                  ) ,
-    ._ft_txe               ( _ft_txe                 ) ,
-    ._ft_rxf               ( _ft_rxf                 ) ,
-    .ft_clk                ( ft_clk                  ) ,
-    ._ft_wr                ( _ft_wr                  ) ,
-    ._ft_siwu              ( _ft_siwu                ) ,
-    ._ft_rd                ( _ft_rd                  ) ,
-    .ft_data               ( ft_data[7:0]            )
-                                                     ) ;
+    .adc_sclk              (  adc_sclk                ),
+    ._adc_cs               ( _adc_cs                  ),
+    .adc_mosi              (  adc_mosi                ),
+    .adc_miso              (  adc_miso                ),
 
-//------------------------------------------------------------------------------
-// Triad Decoder   FSMs to decode triads and map to half-strip hit register
-//------------------------------------------------------------------------------
+    ._pdac_en              ( _pdac_en                 ),
+    .pdac_din              (  pdac_din                ),
+    .pdac_sclk             (  pdac_sclk               ),
+
+    .bx_delay              (  bx_delay[2:0]           ),
+    .pulse_width           (  pulse_width[3:0]        ),
+    .fire_pulse            (  fire_pulse              ),
+    .pulser_ready          (  pulser_ready            ),
+    .triad_persist         (  triad_persist[3:0]      ),
+    .triad_persist1        (  triad_persist1          ),
+
+    .halfstrips            (  halfstrips[31:0]        ),
+    .halfstrips_expect     (  halfstrips_expect[31:0] ),
+    .halfstrips_errcnt     (  halfstrips_errcnt[31:0] ),
+    .halfstrips_errcnt_rst (  halfstrips_errcnt_rst   ),
+    .compout_expect        (  compout_expect          ),
+    .compout_errcnt        (  compout_errcnt          ),
+    .compout_errcnt_rst    (  compout_errcnt_rst      ),
+    .compin_inject         (  compin_inject           ),
+
+    .pktime                (  pktime[2:0]             ),
+    .pkmode                (  pkmode[1:0]             ),
+    .lctrst                (  lctrst                  ),
+
+    ._cdac_en              ( _cdac_en                 ),
+    .cdac_din              (  cdac_din                ),
+    .cdac_sclk             (  cdac_sclk               ),
+
+    .mux_a0                (  mux_a0[15:0]            ),
+    .mux_a1                (  mux_a1[15:0]            ),
+    .mux_a0_next           (  mux_a0_next             ),
+    .mux_a1_next           (  mux_a1_next             ),
+    .mux_a0_prev           (  mux_a0_prev             ),
+    .mux_a1_prev           (  mux_a1_prev             ),
+
+    ._ddd_al               ( _ddd_al                  ),
+    .ddd_mosi              (  ddd_mosi                ),
+    .ddd_miso              (  ddd_miso                ),
+    .ddd_sclk              (  ddd_sclk                ),
+
+    ._reset                (_ft_reset                 ),
+    .clk                   ( clk60                    ),
+    ._serial_wr            (_serial_wr                ),
+    ._serial_rd            (_serial_rd                ),
+
+    ._ft_rxf               (_ft_rxf                   ),
+    ._ft_wr                (_ft_wr                    ),
+    ._ft_rd                (_ft_rd                    ),
+    .ft_byte_out           ( ft_byte_out              ),
+    .ft_byte_in            ( ft_byte_in               ),
+    .serial_sump           ( serial_sump              ));
+
+assign sump = serial_sump;
+
+
+/*
+ * Triad Decoder   FSMs to decode triads and map to half-strip hit register
+ */
 
 wire [7:0] tskip;   // Skipped triads
 
 wire triad_skip;
 assign triad_skip = (|tskip[0]) | (|tskip[1]) | (|tskip[2]) | (|tskip[3]) | (|tskip[4]) | (|tskip[5]);
 
+
 reg   [3:0]   persist  = 0;            // Output persistence-1, ie 5 gives 6-clk width
-reg           persist1 = 1;           // Output persistence is 1, use with  persist=0
+reg           persist1 = 0;            // Output persistence is 1, use with  persist=0
+
+always @ (posedge clk40)
+begin
+    persist  <= (triad_persist-1'b1);
+    persist1 <= (triad_persist1==1 || triad_persist1==0);
+end
 
 genvar idistrip;
 generate
 for (idistrip=0; idistrip<=7; idistrip=idistrip+1)
 begin: distrip_loop
-    triad_decode utriad (clock40, lctrst, persist, persist1,
-    distrip[idistrip],halfstrips[3+idistrip*4:idistrip*4],tskip[idistrip]);
+    triad_decode utriad (
+        .clock          ( clk40                               ),
+        .reset          ( lctrst                              ),
+        .persist        ( persist                             ),
+        .persist1       ( persist1                            ),
+        .triad          ( distrip[idistrip]                   ),
+        .h_strip        ( halfstrips[3+idistrip*4:idistrip*4] ),
+        .triad_skip     ( tskip[idistrip]                     ));
 end
 endgenerate
 
-//------------------------------------------------------------------------------
-// The bitter end
-//------------------------------------------------------------------------------
-
 endmodule
-
-
-//------------------------------------------------------------------------------
-// Decode Triads into a 32bit Distrip Hit Register
-//------------------------------------------------------------------------------
-// reg [2:0] triad0;
-// reg [2:0] triad1;
-// reg [2:0] triad2;
-// reg [2:0] triad3;
-// reg [2:0] triad4;
-// reg [2:0] triad5;
-// reg [2:0] triad6;
-// reg [2:0] triad7;
-//
-// // need to loop in 40Mhz Clock Cycles and assign from distrip_rd[7:0] into
-// // these registers
-//
-// reg [32:0] halfstrips;
-//
-// // Decode Distrip Triad into Half-strip Mapping
-// function [4:0] decode_triad;
-//     input [3:0] triad;
-//     reg   [3:0] hs;
-//     case (triad)
-//         3'b000: hs = 4'b0000;
-//         3'b001: hs = 4'b0000;
-//         3'b010: hs = 4'b0000;
-//         3'b011: hs = 4'b0000;
-//         3'b100: hs = 4'b0001;
-//         3'b101: hs = 4'b0010;
-//         3'b110: hs = 4'b0100;
-//         3'b111: hs = 4'b1000;
-//     endcase
-//     decode_triad [4:0] = hs [4:0];
-// endfunction
-//
-// reg [1:0] i = 0;
